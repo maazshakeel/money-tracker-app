b0110e723765c626dd480456557bf3aaae59e6bf
M. Maaz <maaz.shakeel.bodla@gmail.com>
2023-10-03 02:23:33 +0700

    feat(pirce): add price column in our database

--- api/src/models/transaction.model.ts
+++ api/src/models/transaction.model.ts
@@ -2,6 +2,7 @@ import { Schema, model } from "mongoose";
 
 const TransactionSchema = new Schema({
   name: { type: String, required: true },
+  price: { type: Number, required: true },
   description: { type: String, required: true },
   datetime: { type: Date, required: true },
 });
