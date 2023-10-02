8981429725ad960341f079ce605bc184813011ea
M. Maaz <maaz.shakeel.bodla@gmail.com>
2023-10-02 22:57:56 +0700

    feat(style): style complete

--- src/App.tsx
+++ src/App.tsx
@@ -24,13 +24,13 @@ function App() {
 
       {/* TRANSACTIONS */}
       <div className="transactions">
-        <div className="transaction">
+        <div className="transaction" style={{ borderTop: 0 }}>
           <div className="left">
             <div className="name">New Samsung TV</div>
             <div className="description">was time to buy a new tv</div>
           </div>
           <div className="right">
-            <div className="price">$550</div>
+            <div className="price red">-$550</div>
             <div className="datetime">2023-10-1 15:45</div>
           </div>
         </div>
@@ -39,11 +39,11 @@ function App() {
       <div className="transactions">
         <div className="transaction">
           <div className="left">
-            <div className="name">New Samsung TV</div>
+            <div className="name">Gig job new website</div>
             <div className="description">was time to buy a new tv</div>
           </div>
           <div className="right">
-            <div className="price">$550</div>
+            <div className="price green">+$400</div>
             <div className="datetime">2023-10-1 15:45</div>
           </div>
         </div>
@@ -52,11 +52,11 @@ function App() {
       <div className="transactions">
         <div className="transaction">
           <div className="left">
-            <div className="name">New Samsung TV</div>
+            <div className="name">Sell</div>
             <div className="description">was time to buy a new tv</div>
           </div>
           <div className="right">
-            <div className="price">$550</div>
+            <div className="price green">+$8550</div>
             <div className="datetime">2023-10-1 15:45</div>
           </div>
         </div>
@@ -65,11 +65,11 @@ function App() {
       <div className="transactions">
         <div className="transaction">
           <div className="left">
-            <div className="name">New Samsung TV</div>
+            <div className="name">Bought a iphone15</div>
             <div className="description">was time to buy a new tv</div>
           </div>
           <div className="right">
-            <div className="price">$550</div>
+            <div className="price red">-$1550</div>
             <div className="datetime">2023-10-1 15:45</div>
           </div>
         </div>
